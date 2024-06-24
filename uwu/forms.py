7d4fdb5b11fdc773340a50e4586f317b33028067
PURPLE <purplleee33@gmail.com>
2024-06-24 20:40:37 +0100

    fixed the no affatchment issue

--- uwu/forms.py
+++ uwu/forms.py
@@ -74,10 +74,13 @@ class ModeleForm(FlaskForm):
 
 def file_size_limit(max_size_in_mb):
     max_bytes = max_size_in_mb * 1024 * 1024
+
     def _file_size_limit(form, field):
-        if len(field.data.read()) > max_bytes:
-            raise ValidationError(f'File size must be less than {max_size_in_mb}MB')
-        field.data.seek(0)  # Reset file pointer after read
+        if field.data:
+            if len(field.data.read()) > max_bytes:
+                raise ValidationError(f'File size must be less than {max_size_in_mb}MB')
+            field.data.seek(0)  # Reset file pointer after read
+
     return _file_size_limit
 
 class CommentForm(FlaskForm):
